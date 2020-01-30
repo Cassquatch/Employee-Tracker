5c6404a8eba83373198f377db88a6ea2efb5a1c9
Cassidy Fortner <cassidy.fortner@gmail.com>
2020-01-30 00:31:42 -0600

    have the function calling itself again recursively, still some kinks
    Can continuously call trackEmployees (only function i have in right now is view employees) but everytime it calls, then consoles the table, when i hit the arrow key it erases the consoled table, still need to work on this to figure it out.

--- tracker.js
+++ tracker.js
@@ -1,41 +1,51 @@
 const inquirer = require("inquirer");
 const allEmployees = require("./lib/viewEmployees");
 const dbConnection = require("./db/dbConnection");
-const startTracker = require("./lib/startTracker");
+const manageEmployees = require("./lib/startTracker");
 const ctable = require("console.table");
 
 
 //dbConnection.connection() is returning the createConnection, so db is becoming the connection here
-const db = dbConnection.connection("placeholder_user", "placeholder_pass");
+const db = dbConnection.connection("cassquatch", "mrmeseekslookatme101!");
 
 //assign the startTracker function here to get the initial prompt
-const tracker = startTracker.trackEmployees;
+const tracker = manageEmployees.startTracker;
 
 //use the connection credentials retrieved from dbConnection.connection() to start a new connection
 db.connect((err) => {
-    if (err) throw new Error("Check authentication and privelegs on db");
+    if (err) throw new Error("Check authentication and priveleges on db");
 
     console.log(`Connected as id: ${db.threadId}`);
+     trackEmployees();
+});
+
+//put tracker inside of a function i can recursively call it
+const trackEmployees = () => {
+
     tracker().then((res) => {
         //running a switch to call functions based on choice
         switch (res.action) {
             case "View Departments":
-                //function not implemented yet, just writing skeleton
-                viewDepartments();
+                //function not implemented yet, just writing skeleton passing db so i can use in departments function file
+                viewDepartments(db);
                 break;
-
+    
             case "View Roles":
-                //function not implemented yet, just writing skeleton
-                viewRoles();
+                //function not implemented yet, just writing skeleton passing db so i can use in roles function file
+                viewRoles(db);
                 break;
-
+    
             case "View Employees":
-                allEmployees.viewEmployees();
+                //passing db in here so that i can use it in the view employee function
+                
+                allEmployees.viewEmployees(db);
+                
+                 trackEmployees();
                 break;
-
+    
             case "Exit":
                 db.end();
-
+    
         }
     });
-})
+}
