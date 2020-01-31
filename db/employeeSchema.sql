40c274707e55e564333bdc7f323e2da679323d52
Cassidy Fortner <cassidy.fortner@gmail.com>
2020-01-30 19:12:22 -0600

    reordered schema

--- db/employeeSchema.sql
+++ db/employeeSchema.sql
@@ -4,15 +4,11 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
-CREATE TABLE employee (
-    id INT AUTO_INCREMENT NOT NULL  PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT,
-    manager_id INT, 
-    FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id)
+CREATE TABLE department (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, 
+    name VARCHAR(30)
 );
+
 
 CREATE TABLE role (
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, 
@@ -22,7 +18,12 @@ CREATE TABLE role (
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
-CREATE TABLE department (
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, 
-    name VARCHAR(30)
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT NOT NULL  PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT, 
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
